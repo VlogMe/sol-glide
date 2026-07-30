b9fb38160e518091312da39a6328226435027780
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-07-30 11:27:50 -0600

    Update global-buffer-shim.ts

--- src/lib/global-buffer-shim.ts
+++ src/lib/global-buffer-shim.ts
@@ -1,7 +1,4 @@
 import { Buffer } from "buffer";
-if (typeof globalThis.Buffer === "undefined") {
-  (globalThis as any).Buffer = Buffer;
-}
-if (typeof window !== "undefined" && typeof (window as any).Buffer === "undefined") {
-  (window as any).Buffer = Buffer;
-}
+
+(globalThis as any).Buffer = Buffer;
+(globalThis as any).global = globalThis;
